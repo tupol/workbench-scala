431080af3692aad09f74325d85ba371b955bbfcd
Oliver Tupran <oliver.tupran@gmail.com>
2020-08-10 07:29:46 +0200

    More playing around with the file partitioner

--- src/main/scala/org/tupol/tnp/FilePartitioner.scala
+++ src/main/scala/org/tupol/tnp/FilePartitioner.scala
@@ -1,7 +1,8 @@
 package org.tupol.tnp
 
 import java.io.{FileInputStream, FileOutputStream, RandomAccessFile}
-import java.nio.file.{Files, Path}
+import java.nio.channels.FileChannel
+import java.nio.file.{Files, Path, StandardOpenOption}
 
 import org.tupol.utils.Bracket
 import org.tupol.utils.implicits._
@@ -11,18 +12,24 @@ import scala.util.Try
 
 object FilePartitioner {
 
-  def partitionFile(from: Path, toDir: Path, partitionMarkers: Seq[Long]): Try[Traversable[Path]] =
-    Bracket.auto(new FileInputStream(from.toFile).getChannel()) { f =>
-      Try(Files.createDirectory(toDir))
-      partitionMarkers.sliding(2).zipWithIndex.map { case (sidx +: eidx +: Nil, idx) =>
-        val size = eidx - sidx
-        val targetPath = Files.createFile(toDir.resolve(f"${from.getFileName.toString}.p$idx%04d"))
-        Bracket.auto(new FileOutputStream(targetPath.toFile).getChannel) { t =>
-          t.transferFrom(f, 0, size)
-          targetPath
-        }
-      }.toSeq.allOkOrFail
+  def partitionFile(from: Path, toDir: Path, partitionMarkers: Seq[Long]): Try[Traversable[(Path, Long)]] =
+    Bracket.auto(FileChannel.open(from, StandardOpenOption.READ)) { f =>
+      for {
+        _ <- Try(Files.createDirectory(toDir))
+        results <- partitionMarkers.sliding(2).zipWithIndex.map { case (sidx +: eidx +: Nil, idx) =>
+          val size = eidx - sidx
+          val targetPath = partitionPath(from, toDir, idx)
+          Bracket.auto(FileChannel.open(targetPath, StandardOpenOption.WRITE)) { t =>
+            val txr = t.transferFrom(f, 0, size)
+            // println(s"Transfer from $sidx to $eidx ($size) to $targetPath ($txr)")
+            (targetPath, txr)
+          }
+        }.toSeq.allOkOrFail
+      } yield results
     }.flatten
+
+  def partitionPath(from: Path, toDir: Path, part: Int): Path =
+    Files.createFile(toDir.resolve(f"${from.getFileName.toString}.p$part%04d"))
 
   def mergeFiles(from: Seq[Path], to: Path): Try[Traversable[Long]] = {
     Bracket.auto(new FileOutputStream(to.toFile).getChannel()) { t =>
@@ -35,26 +42,32 @@ object FilePartitioner {
     }.flatten
   }
 
+  def findParSplits(path: Path, pattern: String): Try[Seq[Long]] = {
+    Bracket.auto(new RandomAccessFile(path.toFile, "r")) { raf =>
+      val minSplitSizeBytes = defaultSplitSize(raf.length())
+      findParSplits(raf, minSplitSizeBytes, pattern)
+    }
+  }
   /** Find the indices for split in the given file, such as the split will be done after at least
    * `minSplitSize` and including the given split pattern */
-  def findParSplits(raf: RandomAccessFile, minSplitSize: Int, pattern: String): Seq[Long] = {
+  def findParSplits(raf: RandomAccessFile, minSplitSizeBytes: Long, pattern: String): Seq[Long] = {
     val size = raf.length()
     val x = new Array[Byte](300)
 
     def findParSplits(position: Long, acc: Seq[Long]): Seq[Long] = {
       raf.seek(position)
-      if (position >= size || position + minSplitSize >= size) size +: acc
+      if (position >= size || position + minSplitSizeBytes >= size) size +: acc
       else {
         raf.read(x)
         val res = findFirst(pattern.getBytes, x.iterator)
         if (res == -1) findParSplits(position + x.size, acc)
         else {
-          findParSplits(position + res + pattern.size + minSplitSize, (position + res + pattern.size) +: acc)
+          findParSplits(position + res + pattern.size + minSplitSizeBytes, (position + res + pattern.size) +: acc)
         }
       }
     }
 
-    findParSplits(minSplitSize, Seq(0))
+    findParSplits(minSplitSizeBytes, Seq(0))
   }
 
   /** Returns the position of the first character of the pattern in the input
@@ -68,5 +81,19 @@ object FilePartitioner {
 
     ffp(in.sliding(pattern.size), 0)
   }
+  def defaultSplitSize(fileSize: Long) = math.max((fileSize / 10.0).toLong,  100000)
 
+  def defaultMergeFile(targetDir: Path) = targetDir.resolve(targetDir.getFileName.toString)
+
+  def defaultTargetDir(sourcePath: Path): Path = Files.createTempDirectory(sourcePath.getFileName.toString + ".")
+
+  def deleteRec(root: Path): Unit = {
+    import scala.collection.JavaConverters._
+    if (Files.isDirectory(root)) {
+      Files.list(root).iterator().asScala.foreach(deleteRec)
+      Files.deleteIfExists(root)
+    } else {
+      Files.deleteIfExists(root)
+    }
+  }
 }
--- src/main/scala/org/tupol/tnp/FilePartitionerApp.scala
+++ src/main/scala/org/tupol/tnp/FilePartitionerApp.scala
@@ -5,43 +5,32 @@ import java.nio.file.{Files, Path, Paths}
 
 import org.tupol.utils.timeCode
 
-import scala.concurrent.duration
-import scala.concurrent.duration.MILLISECONDS
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+import scala.concurrent.{Await, Future, duration}
 
 object FilePartitionerApp extends App {
 
   import FilePartitioner._
 
-  val sourcePath = Paths.get("src/test/resources/sample_10G.csv")
-  val raf: RandomAccessFile = new RandomAccessFile(sourcePath.toFile, "r")
+  val sourcePath = Paths.get("src/test/resources/sample_10k.csv")
+//  val raf: RandomAccessFile = new RandomAccessFile(sourcePath.toFile, "r")
   val targetFolder = Paths.get(s"/tmp/test1")
-  val targetFile = targetFolder.resolve("file_00")
-
+  val targetFile = defaultMergeFile(targetFolder)
   println("============================================================")
-  deleteRec(targetFolder)
-  val (partitionMarkers, rt10) = timeCode(findParSplits(raf, 1000000000, "\n").reverse)
+  println(s"input file:             $sourcePath")
+  println(s"output splits folder:   $targetFolder")
+  println(s"output merged file:     $targetFile")
+  println("============================================================")
+  val (partitionMarkers, rt10) = timeCode(findParSplits(sourcePath, "\n").get.reverse)
   println("findParSplits: " + duration.FiniteDuration(rt10, MILLISECONDS))
+  deleteRec(targetFolder)
   val (partFiles, rt11) = timeCode(partitionFile(sourcePath, targetFolder, partitionMarkers).get)
   println("partitionFile: " + duration.FiniteDuration(rt11, MILLISECONDS))
-  val (_, rt12) = timeCode(mergeFiles(partFiles.toSeq, targetFile).get)
+  val (_, rt12) = timeCode(mergeFiles(partFiles.map(_._1).toSeq, targetFile).get)
   println("mergeFiles:    " + duration.FiniteDuration(rt12, MILLISECONDS))
   val (_, rt20) = timeCode(Files.copy(sourcePath, targetFolder.resolve(s"original-${sourcePath.getFileName}")))
   println("copyFiles:     " + duration.FiniteDuration(rt20, MILLISECONDS))
 
-  println("============================================================")
 
-
-  def defaultTargetDir(sourcePath: Path): Path = Files.createTempDirectory(sourcePath.getFileName.toString + ".")
-
-
-  def deleteRec(root: Path): Unit = {
-    import scala.collection.JavaConverters._
-    if (Files.isDirectory(root)) {
-      Files.list(root).iterator().asScala.foreach(deleteRec)
-      Files.deleteIfExists(root)
-    } else {
-      Files.deleteIfExists(root)
-    }
-  }
 
 }
